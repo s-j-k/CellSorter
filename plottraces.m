4d2c15f47f127b160b502d3f77b9c9a785494832
su <68665480+s-j-k@users.noreply.github.com>
2022-03-28 10:54:51 -0400

    Update plottraces.m

--- plottraces.m
+++ plottraces.m
@@ -1,11 +1,11 @@
 figure; hold on;
 nTraces=size(d_pre,1);
-offset=3;
+offset=5;
 % for i = 1:nTraces
 for i = 1:nTraces
     plot(1:size(d_pre,2),d_pre(i,:)+i*offset - offset)
 end
-ylim([-5 120])
+ylim([-5 200])
 %% now sort
 figure;hold on;
 [maxF,ind] = max(d_pre, [], 2);
@@ -15,7 +15,11 @@ cc = d_pre(index, :);
 for i = 1:nTraces
     plot(1:size(d_pre,2),cc(i,1:size(d_pre,2))+i*offset - offset)
 end
-ylim([-5 125])
+ylim([-5 200])
+%%
+% now normalize the traces ? 
+
+
 %%
 figure; hold on;
 imagesc(cc)
